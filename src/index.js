795ae505aa09d352436e80289f869a33724a495c
mikederycke <mike.derycke@ehb.be>
2023-04-06 13:04:17 +0200

    Basic routing added  RouterProvider/BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './routes/Home';
+import About from './routes/About';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-
-
-
+const router = createBrowserRouter([
+  { 
+    path: '/', 
+    element: <Home /> 
+  },{
+    path: '/about',
+    element: <About />
+  }
+])
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
